f5b0d28cff0141ad3cd909dabfddf73639a7f038
kinyashov <kinyashov@gmail.com>
2017-11-24 18:43:35 +1000

    add puppy recipe parse

---
+++ Parsing/Recipe_Puppy_Parse.py
@@ -0,0 +1,19 @@
+import urllib.request
+import json
+import html.parser
+
+
+i = input("Enter ingredients (separated by commas): ").replace(' ', '')
+
+if i != '':
+    URL = "http://www.recipepuppy.com/api/?i={}".format(i)
+    with urllib.request.urlopen(URL) as url:
+        data = url.read().decode()
+    data = html.unescape(data)
+    data = json.loads(data)
+    for result in data['results']:
+        print(' '.join(result['title'].split()))
+else:
+    print('Not entered ingredients')
+
+
