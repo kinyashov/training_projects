719df4105312f42e35acf137717124b1004ae30c
kinyashov <kinyashov@gmail.com>
2017-11-26 13:06:28 +1000

    add check for token, add formatting, optimized strings

--- Parsing/parse_football.py
+++ Parsing/parse_football.py
@@ -1,8 +1,16 @@
 import urllib.request
 import json
 
+file = 'api_token'
+try:
+    with open(file, 'r') as api_token:
+        token = api_token.readline()
+except FileNotFoundError:
+    token = ''
+
 leagues = [445, 450, 452, 455, 456]
-headers = {'X-Auth-Token': '', 'X-Response-Control': 'minified'}
+headers = {'X-Auth-Token': token, 'X-Response-Control': 'minified'}
+
 for league in leagues:
     url = 'http://api.football-data.org/v1/competitions/{}/leagueTable'.format(league)
     req = urllib.request.Request(url, headers=headers)
@@ -12,8 +20,8 @@ for league in leagues:
     data = json.loads(data)
     teams = sorted(data['standing'], key=lambda x: x['goals'], reverse=True)
 
-    print('\n', data['leagueCaption'])
-    print('Pos', 'Team'.ljust(14), 'Goals')
+    print('\n\x1b[32m{}\x1b[0m'.format(data['leagueCaption']))
+    print('\x1b[4m{:<4}{:<15}{}\x1b[0m'.format('Pos', 'Team', 'Goals'))
     for pos, row in enumerate(teams, 1):
         if pos < 6:
-            print('{}.  {}{}'.format(pos, row['team'].ljust(15), row['goals']))
+            print('{:<4}{:<15}{}'.format(pos, row['team'], row['goals']))
