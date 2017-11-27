eaf5fc5f3c12d5d843fdef656e9e440701598d5c
kinyashov <kinyashov@gmail.com>
2017-11-27 12:30:04 +1000

    added printing in output file

--- Parsing/ISS/parse_ISS.py
+++ Parsing/ISS/parse_ISS.py
@@ -13,5 +13,6 @@ dddddd_lat = data['iss_position']['latitude']
 dddddd_long = data['iss_position']['longitude']
 ddmmss_lat, ddmmss_long = format_coord.ddmmss_format(float(dddddd_lat), float(dddddd_long))
 
-print('DDDDDD format: {} {}'.format(dddddd_lat, dddddd_long))
-print('DDMMSS format: {} {}'.format(ddmmss_lat, ddmmss_long))
+with open('output', 'w') as output_file:
+    print('DDDDDD format: {} {}'.format(dddddd_lat, dddddd_long), file=output_file)
+    print('DDMMSS format: {} {}'.format(ddmmss_lat, ddmmss_long), file=output_file)
