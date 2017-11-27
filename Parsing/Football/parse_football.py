b6b31dcc5d156cc1628152dfc99136eb94bf06a7
kinyashov <kinyashov@gmail.com>
2017-11-27 12:26:32 +1000

    added printing in output file

--- Parsing/Football/parse_football.py
+++ Parsing/Football/parse_football.py
@@ -11,16 +11,17 @@ except FileNotFoundError:
 leagues = [445, 450, 452, 455, 456]  # id leagues in database
 headers = {'X-Auth-Token': token, 'X-Response-Control': 'minified'}  
 
-for league in leagues:
-    url = 'http://api.football-data.org/v1/competitions/{}/leagueTable'.format(league)
-    req = urllib.request.Request(url, headers=headers)
-    with urllib.request.urlopen(req) as url:
-        data = url.read().decode()
-    data = json.loads(data)
-    teams = sorted(data['standing'], key=lambda x: x['goals'], reverse=True)
+with open('output', 'w') as output_file:
+    for league in leagues:
+        url = 'http://api.football-data.org/v1/competitions/{}/leagueTable'.format(league)
+        req = urllib.request.Request(url, headers=headers)
+        with urllib.request.urlopen(req) as url:
+            data = url.read().decode()
+        data = json.loads(data)
+        teams = sorted(data['standing'], key=lambda x: x['goals'], reverse=True)
 
-    print('\n\x1b[32m{}\x1b[0m'.format(data['leagueCaption']))
-    print('\x1b[4m{:<4}{:<15}{}\x1b[0m'.format('Pos', 'Team', 'Goals'))
-    for pos, row in enumerate(teams, 1):
-        if pos < 6:
-            print('{:<4}{:<15}{}'.format(pos, row['team'], row['goals']))
+        print('\n{}'.format(data['leagueCaption']), file=output_file)
+        print('{:<4}{:<15}{}'.format('Pos', 'Team', 'Goals'), file=output_file)
+        for pos, row in enumerate(teams, 1):
+            if pos < 6:
+                print('{:<4}{:<15}{}'.format(pos, row['team'], row['goals']), file=output_file)
