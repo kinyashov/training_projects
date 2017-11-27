e303c836be4e8cd315e04c8d796e9a97ac58540b
kinyashov <kinyashov@gmail.com>
2017-11-27 10:21:20 +1000

    added abilities in output, added exception not found pokemon

--- Parsing/parse_pokemon.py
+++ Parsing/parse_pokemon.py
@@ -2,6 +2,7 @@ import urllib.request
 import json
 
 pokename = input('Pokemon: ')
+
 proxies = {'https': '51.15.202.250:3128'}
 headers = {'User-Agent': 'Mozilla/5.0'}
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokename)
@@ -11,11 +12,27 @@ proxy = urllib.request.ProxyHandler(proxies=proxies)
 opener = urllib.request.build_opener(proxy)
 urllib.request.install_opener(opener)
 
-with urllib.request.urlopen(req) as url:
-    response = url.read().decode()
-response = json.loads(response)
+try:
+    with urllib.request.urlopen(req) as url:
+        response = url.read().decode()
+    response = json.loads(response)
 
-print('id: {}\nweight: {}\nheight: {}\nbase experience: {}'.format(response['id'],
-                                                                   response['weight'],
-                                                                   response['height'],
-                                                                   response['base_experience']))
+    abilities = []
+    for ab in response['abilities']:
+        if not ab['is_hidden']:
+            abilities.append(ab['ability']['name'])
+
+    hidden_abilities = []
+    for ab in response['abilities']:
+        if ab['is_hidden']:
+            hidden_abilities.append(ab['ability']['name'])
+
+    print('id: {}'.format(response['id']),
+          '\nweight: {}'.format(response['weight']),
+          '\nheight: {}'.format(response['height']),
+          '\nbase experience: {}'.format(response['base_experience']),
+          '\nabilities: {}'.format(', '.join(abilities)),
+          '\nhidden abilities: {}'.format(', '.join(hidden_abilities)))
+
+except urllib.error.HTTPError:
+    print('Pokemon not found')
