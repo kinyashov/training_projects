c6b6e6bef5b57538df8422904a83ab8ac4b39c8c
kinyashov <kinyashov@gmail.com>
2017-11-27 13:04:33 +1000

    added printing response in output file, deleted ingredients in response

--- Parsing/Recipe/parse_recipe.py
+++ Parsing/Recipe/parse_recipe.py
@@ -2,24 +2,28 @@ import urllib.request
 import json
 import html.parser
 
-
-ingredients = input("Enter ingredients (separated by commas): ").replace(' ', '')
+try:
+    with open('ingredients_for_test', 'r') as test_file:
+        ingredients = test_file.readlines()
+except FileNotFoundError:
+    ingredients = input("Enter ingredients (separated by commas): ").replace(' ', '')
 
 if ingredients != '':
 
-    URL = "http://www.recipepuppy.com/api/?i={}".format(ingredients)
+    URL = "http://www.recipepuppy.com/api/?i={}".format(''.join(ingredients))
 
-    with urllib.request.urlopen(URL) as url:
-        data = url.read().decode()
+    with urllib.request.urlopen(URL) as req:
+        data = req.read().decode()
     data = html.unescape(data)
     data = json.loads(data)
 
-    if len(data['results']) == 0:
-        print('Recipes not found')
-    else:
-        for recipe in data['results']:
-            print('\x1b[1m{}\x1b[0m: {}'.format(' '.join(recipe['title'].split()),
-                                                recipe['ingredients']))
+    with open('output', 'w') as output_file:
+        if len(data['results']) == 0:
+            print('Recipes not found', file=output_file)
+        else:
+            for recipe in data['results']:
+                print('{}'.format(' '.join(recipe['title'].split())),
+                      file=output_file)
 
 else:
     print('Not entered ingredients')
