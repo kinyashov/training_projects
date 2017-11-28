8e9c036a262ffa3cbd8e195256212a7091a22e46
kinyashov <kinyashov@gmail.com>
2017-11-28 12:02:39 +1000

    added link for google and yandex maps, joined all functions print in one function

--- Parsing/ISS/parse_ISS.py
+++ Parsing/ISS/parse_ISS.py
@@ -12,7 +12,16 @@ data = json.loads(data)
 dddddd_lat = data['iss_position']['latitude']
 dddddd_long = data['iss_position']['longitude']
 ddmmss_lat, ddmmss_long = format_coord.ddmmss_format(float(dddddd_lat), float(dddddd_long))
+yandex_map = 'https://yandex.ru/maps/?mode=whatshere&whatshere%5Bpoint%5D=' \
+             '{}%2C{}&whatshere%5Bzoom%5D=NaN'.format(dddddd_long, dddddd_lat)
+google_map = 'https://www.google.ru/maps/place/{}+{}/'.format(ddmmss_lat, ddmmss_long)
 
 with open('output', 'w') as output_file:
-    print('DDDDDD format: {} {}'.format(dddddd_lat, dddddd_long), file=output_file)
-    print('DDMMSS format: {} {}'.format(ddmmss_lat, ddmmss_long), file=output_file)
+    print('DDDDDD format: {} {} \
+          \nDDMMSS format: {} {} \
+          \nYandex Map: {} \
+          \nGoogle Map: {}'.format(dddddd_lat, dddddd_long,
+                                   ddmmss_lat, ddmmss_long,
+                                   yandex_map,
+                                   google_map),
+          file=output_file)
