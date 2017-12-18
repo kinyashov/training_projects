e699c33286f920293d424a3190424f5b57c266d7
kinyashov <kinyashov@gmail.com>
2017-12-18 14:06:55 +1000

    added comments, minor improvements

--- Habrapars/parsing.py
+++ Habrapars/parsing.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import urllib3
 from html.parser import HTMLParser
-import timeformat
+import utyls
 import psycopg2
 
+
+# crutches for HTMLParser, forgive me
+# global vars allow to track desired data
 is_author = False
 is_id = False
 is_date = False
@@ -12,9 +17,10 @@ dict_title_table = {}
 dict_hub_table = {}
 
 
-def update_base():
+def update_base(page=12):
     with psycopg2.connect("dbname=habradata") as conn:
         with conn.cursor() as cur:
+            # two tables to simplify SELECT
             cur.execute("""
                         CREATE TABLE IF NOT EXISTS habrapost (
                         id text PRIMARY KEY,
@@ -67,12 +73,14 @@ def update_base():
                         dict_title_table['author'] = data
                         is_author = False
                     if is_date is True:
-                        data = timeformat.habratime_isotime(data)
+                        data = utyls.habratime_isotime(data)
                         dict_title_table['date'] = data
                         is_date = False
                     if is_title is True:
                         dict_title_table['title'] = data
                         is_title = False
+                        # tuple completed for INSERT INTO habrapost
+                        # further, tuple will be overwrite
                         cur.execute("""
                                     INSERT INTO habrapost (id, author, time, title) 
                                     VALUES (%s, %s, %s, %s)
@@ -87,6 +95,7 @@ def update_base():
                     if is_hub is True:
                         dict_hub_table['hub'] = data
                         is_hub = False
+                        # tuple completed for INSERT INTO habrahub
                         cur.execute("""
                                     INSERT INTO habrahub (hub, id) 
                                     VALUES (%s, %s);
@@ -97,7 +106,7 @@ def update_base():
                         conn.commit()
 
             http = urllib3.PoolManager()
-            for i in range(1, 12):
+            for i in range(1, page):
                 r = http.request('GET', 'https://habrahabr.ru/top/monthly/page{}/'.format(i))
                 res = r.data.decode('utf-8')
                 parser = MyHTMLParser()
