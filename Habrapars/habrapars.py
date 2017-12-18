eada7518e3443a5faba475c2a9ce41d28d1cdd31
kinyashov <kinyashov@gmail.com>
2017-12-18 13:37:45 +1000

    added some answer, added helper for parseargs

--- Habrapars/habrapars.py
+++ Habrapars/habrapars.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import argparse
 from datetime import datetime, timedelta
 import psycopg2
@@ -7,9 +9,20 @@ import parsing
 valid_date = lambda dt: datetime.strptime(dt, "%Y-%m-%d")
 
 parser = argparse.ArgumentParser(description='list of title by hub')
-parser.add_argument('string', type=str, nargs='*')
-parser.add_argument('-a', '--after', type=valid_date, default=datetime.today() - timedelta(days=7))
-parser.add_argument('-b', '--before', type=valid_date, default=datetime.today())
+parser.add_argument('string', type=str, nargs='*',
+                    help="""'top post' - popular post from week;
+                         'top hub' - top 10 popular hub in habradata;
+                         'top author' - top 10 productive author;
+                         <name of hub> [-a|--after YYYY-MM-DD] [-b|--before YYYY-MM-DD]
+                         """)
+parser.add_argument('-a', '--after',
+                    type=valid_date,
+                    default=datetime.today() - timedelta(days=7),
+                    help='starting datetime')
+parser.add_argument('-b', '--before',
+                    type=valid_date,
+                    default=datetime.today(),
+                    help='ending datetime')
 
 args = parser.parse_args()
 
@@ -52,9 +65,17 @@ with psycopg2.connect("dbname=habradata") as conn:
                     for result in results:
                         print(*result)
                 else:
-                    print('я вас не понял')
+                    print("unknown command"
+                          "\nenter '-h' for help")
         elif args.string[0] == 'update':
-            parsing.update_base()
+            try:
+                if len(args.string) > 1:
+                    parsing.update_base(int(args.string[1]))
+            except ValueError:
+                print('ERROR: second argument is not integer')
+            else:
+                parsing.update_base()
+            print('database is updated')
         elif type(args.string[0]) is str:
             cur.execute("""
                         SELECT title
@@ -69,5 +90,10 @@ with psycopg2.connect("dbname=habradata") as conn:
                          args.before,
                          args.string[0]))
             results = cur.fetchall()
-            for result in results:
-                print(*result)
+            if len(results) == 0:
+                print("nothing found at your request"
+                      "\ncheck command or name of hub"
+                      "\nor enter '-h' for help")
+            else:
+                for result in results:
+                    print(*result)
