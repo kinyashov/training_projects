f53f88110fe5d3b5f83a4f98ccde178cfe99ff42
Александр Киняшов <kinyashov@gmail.com>
2018-01-22 14:44:29 +1000

    cleared installed apps

--- django_projects/first_project/first_site/first_site/settings.py
+++ django_projects/first_project/first_site/first_site/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'library',
     'musli',
-    'shop',
-    'textbox'
 ]
 
 MIDDLEWARE = [
