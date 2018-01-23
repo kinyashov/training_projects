01f2f206ac0438e25bce8e29c682588f3b0235d8
Александр Киняшов <kinyashov@gmail.com>
2018-01-23 14:02:00 +1000

    added 3 path for sort in performer's, genre's and album's pages

--- django_projects/first_project/first_site/musli/urls.py
+++ django_projects/first_project/first_site/musli/urls.py
@@ -5,10 +5,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('performer/', views.all_performer, name='all performers'),
+    path('performer/', views.all_performer, name='all_performer'),
+    path('performer/?sort=<sort>', views.all_performer, name='all_performer'),
     path('performer/<slug:slug_performer>/', views.performer, name='performer'),
     path('genre/', views.all_genre, name='all_genre'),
+    path('genre/?sort=<sort>', views.all_genre, name='all_genre'),
     path('genre/<slug:slug_genre>/', views.genre, name='genre'),
     path('album/', views.all_album, name='all_album'),
+    path('album/?sort=<sort>', views.all_album, name='all_album'),
     path('album/<slug:slug_album>/', views.album, name='album'),
 ]
